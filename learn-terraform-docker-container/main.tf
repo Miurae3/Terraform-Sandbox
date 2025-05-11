e12fa855916af88446103428411420b5d7fe1202
miura <miura.hublab@gmail.com>
2025-05-10 22:27:16 -0300

    Criação automatizada de container Nginx docker via terraform

---
+++ learn-terraform-docker-container/main.tf
@@ -0,0 +1,27 @@
+#Criando um container Nginx via docker/terraform
+
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "nginx-container-terraform"
+
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
